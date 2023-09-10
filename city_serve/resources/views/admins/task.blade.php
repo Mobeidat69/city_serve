33be6f118246246280164fc9180a056b224c4618
mariaali7 <mariaalbasyouni@gmail.com>
2023-09-10 15:47:21 +0300

    m

--- city_serve/resources/views/admins/task.blade.php
+++ city_serve/resources/views/admins/task.blade.php
@@ -2,59 +2,8 @@
 @extends('layouts.admin')
 
 @section('content')
-{{-- <div class="container">
-    <form>
-        <div class="form-group">
-            <label for="id">ID:</label>
-            <input type="text" class="form-control" id="id" value="{{ $task->id }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="title">Title:</label>
-            <input type="text" class="form-control" id="title" value="{{ $task->title }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="description">Description:</label>
-            <textarea class="form-control" id="description" disabled>{{ $task->description }}</textarea>
-        </div>
-        <div class="form-group">
-            <label for="location">Location:</label>
-            <input type="text" class="form-control" id="location" value="{{ $task->location }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="start_date">Start Date:</label>
-            <input type="text" class="form-control" id="start_date" value="{{ $task->start_date }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="end_date">End Date:</label>
-            <input type="text" class="form-control" id="end_date" value="{{ $task->end_date }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="deadline">Deadline:</label>
-            <input type="text" class="form-control" id="deadline" value="{{ $task->deadline }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="skills_required">Skills Required:</label>
-            <input type="text" class="form-control" id="skills_required" value="{{ $task->skills_required }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="vacancy">Vacancy:</label>
-            <input type="text" class="form-control" id="vacancy" value="{{ $task->vacancy }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="category_id">Category ID:</label>
-            <input type="text" class="form-control" id="category_id" value="{{ $task->category_id }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="image">Image:</label>
-            <br>
-            <img src="{{ $task->image }}" alt="{{ $task->id }}-picture" style="max-width: 200px">
-        </div>
-        <div class="form-group">
-            <label for="status">Status:</label>
-            <input type="text" class="form-control" id="status" value="{{ $task->status }}" disabled>
-        </div>
-    </form>
-</div> --}}
+
+
 
 <div class="container">
     <div class="row">
